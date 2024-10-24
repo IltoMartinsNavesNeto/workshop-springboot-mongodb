0445b12535a372033882585574bcb12977b731b1
IltoMartinsNavesNeto <iltoneto400@gmail.com>
2024-10-24 11:07:03 -0300

    Acrescentando comentários aos posts

---
+++ src/main/java/com/iltoneto/workshopmongo/services/PostService.java
@@ -0,0 +1,20 @@
+package com.iltoneto.workshopmongo.services;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.iltoneto.workshopmongo.domain.Post;
+import com.iltoneto.workshopmongo.respository.PostRepository;
+import com.iltoneto.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
